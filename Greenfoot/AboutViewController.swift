369a32512daf27394075b1c5a8e3f95a9109195f
Anmol Parande <parandea1.7@gmail.com>
2017-12-16 18:47:06 -0600

    Moved the Scroll Views out of IB so they are completely programmatic now.

--- Greenfoot/AboutViewController.swift
+++ Greenfoot/AboutViewController.swift
@@ -58,7 +58,9 @@ class AboutViewController: UICollectionViewController, UICollectionViewDelegateF
         let paddingSpace = sectionInsets.left * (itemsPerRow+1)
         let availableWidth = view.frame.width - paddingSpace
         let widthPerItem = availableWidth / itemsPerRow
-        let heightPerItem = 0.66 * widthPerItem
+        
+        let modifier:CGFloat = (UIDevice.current.userInterfaceIdiom == .phone) ? 0.66 : 0.5
+        let heightPerItem = modifier * widthPerItem
         
         return CGSize(width: widthPerItem, height: heightPerItem)
     }
@@ -89,13 +91,14 @@ class AboutViewController: UICollectionViewController, UICollectionViewDelegateF
     }
     
     @objc func showLicense(_ sender: AnyObject) {
-        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "licenseController")
+        let contentHeight:CGFloat = (UIDevice.current.userInterfaceIdiom == .pad) ? 1000 : 2000
+        let vc = FileScrollViewController(fileName: "license", contentHeight: contentHeight)
         self.navigationController?.pushViewController(vc, animated: true)
     }
     
     @objc func showPrivacy(_ sender: AnyObject) {
-        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "privacyController") as! ScrollingController
-        vc.scrollHeight = 5000
+        let contentHeight:CGFloat = (UIDevice.current.userInterfaceIdiom == .pad) ? 2500 : 4000
+        let vc = FileScrollViewController(fileName: "privacy", contentHeight: contentHeight)
         self.navigationController?.pushViewController(vc, animated: true)
     }
     
@@ -143,26 +146,60 @@ class AboutFooter: UICollectionReusableView {
     @IBOutlet weak var privacyButton: UIButton!
 }
 
-class ScrollingController: UIViewController {
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var scrollingLabel: UILabel!
+class FileScrollViewController: UIViewController {
+    var scrollHeight:CGFloat
+    var file: String
     
-    var scrollHeight:CGFloat = 2000
+    var scrollingLabel: UILabel!
+    var scrollView: UIScrollView!
+    var contentView: UIView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    init(fileName: String, contentHeight: CGFloat) {
+        self.file = fileName
+        self.scrollHeight = contentHeight
         
-        navigationItem.backButton.tintColor = UIColor.white
+        super.init(nibName: nil, bundle: nil)
     }
     
-    override func viewDidLayoutSubviews() {
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        scrollView = UIScrollView(frame: UIScreen.main.bounds)
         
         let size = CGSize(width: UIScreen.main.bounds.width, height: scrollHeight)
         scrollView.contentSize = size
         
-        scrollingLabel.frame = CGRect(x: 10, y: 0, width: size.width-20, height: size.height)
-        scrollingLabel.sizeToFit()
+        let frame = CGRect(x: 10, y: 0, width: size.width-20, height: size.height)
         
-        scrollView.layoutIfNeeded()
+        contentView = UIView(frame: frame)
+        scrollingLabel = UILabel(frame: CGRect(origin: CGPoint.zero, size: CGSize(width:size.width-20, height:size.height)))
+        
+        contentView.addSubview(scrollingLabel)
+        scrollView.addSubview(contentView)
+       
+        self.view = scrollView
+    }
+    
+    override func viewDidLoad() {
+        navigationItem.backButton.tintColor = UIColor.white
+        
+        if let filepath = Bundle.main.path(forResource: file, ofType: "txt") {
+            do {
+                let contents = try String(contentsOfFile: filepath)
+                scrollingLabel.text = contents
+                scrollingLabel.numberOfLines = 0
+                scrollingLabel.textColor = UIColor.black
+                
+                scrollingLabel.sizeToFit()
+            } catch {
+                // contents could not be loaded
+                print("Couldn't load file")
+            }
+        } else {
+            // example.txt not found!
+            print("File not found")
+        }
     }
 }
