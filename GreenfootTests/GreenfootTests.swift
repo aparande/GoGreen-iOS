e1132e21da368eb697740c61440ec488370e6bed
Anmol Parande <parandea1.7@gmail.com>
2018-03-16 20:37:53 -0500

    Added Tests

---
+++ GreenfootTests/GreenfootTests.swift
@@ -0,0 +1,29 @@
+//
+//  GreenfootTests.swift
+//  GreenfootTests
+//
+//  Created by Anmol Parande on 3/15/18.
+//  Copyright © 2018 Anmol Parande. All rights reserved.
+//
+
+import XCTest
+@testable import Greenfoot
+
+class GreenfootTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        testingModal = nil
+        super.tearDown()
+    }
+    
+    func testPointConsensus() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
