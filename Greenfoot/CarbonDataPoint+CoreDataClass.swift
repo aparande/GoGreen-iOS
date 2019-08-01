91988f33289cdb0137a82c3d632e926b5f22bcc9
Anmol Parande <parandea1.7@gmail.com>
2019-07-31 22:27:59 -0700

    Create method for CarbonDataPoints

--- Greenfoot/CarbonDataPoint+CoreDataClass.swift
+++ Greenfoot/CarbonDataPoint+CoreDataClass.swift
@@ -12,5 +12,30 @@ import CoreData
 
 @objc(CarbonDataPoint)
 public class CarbonDataPoint: NSManagedObject, CoreDataRecord {
+    required convenience init?(inContext context: NSManagedObjectContext,
+                              source: CarbonSource,
+                              unit: CarbonUnit,
+                              month: NSDate,
+                              value: Double) {
+        self.init(context: context)
+        self.source = source
+        self.unit = unit
+        self.month = month
+        self.lastUpdated = NSDate()
+        self.rawValue = value
+        
+        guard let possibleConversions = self.unit.conversionsTo else {
+            print("Couldn't create CarbonDataPoint because Unit has no conversions")
+            context.delete(self)
+            return nil
+        }
 
+        guard let carbonConversion = possibleConversions.first(where: {($0 as? Conversion)?.dest.fid == "direct-default"}) as? Conversion else {
+            print("Couldn't create CarbonDataPoint because Unit has no conversion to Carbon")
+            context.delete(self)
+            return nil
+        }
+        
+        self.carbonValue = carbonConversion.convert(value)
+    }
 }
---
+++ GreenfootTests/CoreDataTests/CarbonDataPointTest.swift
@@ -0,0 +1,38 @@
+//
+//  CarbonDataPointTest.swift
+//  GreenfootTests
+//
+//  Created by Anmol Parande on 7/31/19.
+//  Copyright © 2019 Anmol Parande. All rights reserved.
+//
+
+import XCTest
+@testable import Greenfoot
+
+class CarbonDataPointTest: CoreDataTest {
+    func testCreate() {
+        do {
+            let electricSource = try CarbonSource.all(inContext: dbManager.backgroundContext, withTypes: [.electricity]).first
+            XCTAssertNotNil(electricSource)
+            
+            let unitRequest = CarbonUnit.fetchAllRequest
+            unitRequest.predicate = NSPredicate(format: "(%K = %@) AND (%K = %@)", argumentArray: ["sourceType", electricSource!.sourceType.rawValue, "fid", "electric-default"])
+            let unit = try dbManager.backgroundContext.fetch(unitRequest).first
+            XCTAssertNotNil(unit)
+            
+            let dataPoint = CarbonDataPoint(inContext: dbManager.backgroundContext, source: electricSource!, unit: unit!, month: NSDate(), value: 500)
+            
+            print(electricSource)
+            
+            XCTAssertNotNil(dataPoint)
+            XCTAssert(dataPoint?.carbonValue == 616.4)
+            
+            let sourceData = electricSource?.data?.filter {type(of: $0) == CarbonDataPoint.self}
+            
+            XCTAssert(sourceData?.count == 1) //The Reference
+        } catch {
+            XCTFail()
+        }
+        
+    }
+}
