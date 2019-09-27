a136ed4cb88222d9647529ac0abf4a896c7602a0
Anmol Parande <parandea1.7@gmail.com>
2019-09-27 00:10:21 -0700

    Stopped crash when BulkDataLoader is not loaded from storyboard

--- Greenfoot/BulkDataViewController.swift
+++ Greenfoot/BulkDataViewController.swift
@@ -18,7 +18,7 @@ class BulkDataViewController: UITableViewController {
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
     
     override func viewDidLoad() {
